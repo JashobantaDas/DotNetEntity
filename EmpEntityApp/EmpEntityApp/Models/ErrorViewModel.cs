66eee438c8b06bb2518ce2fcbb5ddf0fb497a523
Jashobantdas <dbapun569@gmail.com>
2024-04-12 00:29:51 +0530

    HII

---
+++ EmpEntityApp/EmpEntityApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmpEntityApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
